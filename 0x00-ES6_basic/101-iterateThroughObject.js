e807e446c93e6a9a428c11977afbf2833f37bd2e
dejetem <enyeoluwarotimisimon@gmail.com>
2023-01-24 00:57:54 +0100

    task 15 update

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return Object.split('|');
+  const newArray = [];
+  for (const array of reportWithIterator) {
+    newArray.push(array);
+  }
+  return newArray.join('|');
 }
