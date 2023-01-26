14cfa953746864fdd58dcca3e05fd0963e031a8e
dejetem <enyeoluwarotimisimon@gmail.com>
2023-01-26 22:25:31 +0100

    update

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-export class HolbertonClass {
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,11 +13,16 @@ export class HolbertonClass {
   }
 }
 
-export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+export default HolbertonClass;
+
+class StudentHolberton extends HolbertonClass {
+  constructor(firstName, lastName) {
+    super(HolbertonClass);
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -25,16 +30,13 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this.holbertonClass;
   }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
