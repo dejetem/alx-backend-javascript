cfa6011254aeea4c9534f37f74ea0ce1fbb661f4
dejetem <enyeoluwarotimisimon@gmail.com>
2023-01-26 21:19:21 +0100

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const clsRooms = [];
+  clsRooms.push(new ClassRoom(19));
+  clsRooms.push(new ClassRoom(20));
+  clsRooms.push(new ClassRoom(34));
+  return clsRooms;
+}
