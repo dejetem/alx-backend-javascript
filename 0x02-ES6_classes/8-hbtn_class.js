97ab5eb8beda104ab446d60ed6bd0c41dcb45f86
dejetem <enyeoluwarotimisimon@gmail.com>
2023-01-26 21:54:36 +0100

    task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,13 @@
+export default class HolbertonClass {
+  constructor(size = 0, location = '') {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](atr) {
+    if (atr === 'number') {
+      return `${this._size}`;
+    }
+    return `${this._location}`;
+  }
+}
